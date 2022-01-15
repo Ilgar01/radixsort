c31f8f7120d596b68a56f1043bf0c66c1a3c6daa
Ilgar01 <haciyevilqar0101@gmail.com>
2022-01-15 15:16:03 +0400

    remove size

--- src/main/java/az/tasks/RadixSort.java
+++ src/main/java/az/tasks/RadixSort.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class RadixSort {
     int counter=0;
-    void countingSort(int array[], int size, int place) {
-        int[] output = new int[size + 1];
+    void countingSort(int array[], int place) {
+        int[] output = new int[array.length + 1];
         int max = array[0];
-        for (int i = 1; i < size; i++) {
+        for (int i = 1; i < array.length; i++) {
             if (array[i] > max)
                 max = array[i];
             counter++;
@@ -18,37 +18,36 @@ public class RadixSort {
             count[i] = 0;
            counter++;
 
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < array.length; i++)
             count[(array[i] / place) % 10]++;
            counter++;
         for (int i = 1; i < 10; i++)
             count[i] += count[i - 1];
             counter++;
 
-        for (int i = size - 1; i >= 0; i--) {
+        for (int i = array.length - 1; i >= 0; i--) {
             output[count[(array[i] / place) % 10] - 1] = array[i];
             count[(array[i] / place) % 10]--;
             counter++;
         }
 
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < array.length; i++)
             array[i] = output[i];
             counter++;
     }
 
-    int getMax(int array[], int n) {
+    int getMax(int array[]) {
         int max = array[0];
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i < array.length; i++)
             if (array[i] > max)
                 max = array[i];
                counter++;
         return max;
     }
-    void radixSort(int array[], int size) {
-        // Get maximum element
-        int max = getMax(array, size);
+    void radixSort(int array[]) {
+        int max = getMax(array);
         for (int place = 1; max / place > 0; place *= 10)
-            countingSort(array, size, place);
+            countingSort(array, place);
     }
 
     public static void main(String args[]) {
@@ -56,7 +55,7 @@ public class RadixSort {
         System.out.println(Arrays.toString(data));
         int size = data.length;
         RadixSort rs = new RadixSort();
-        rs.radixSort(data, size);
+        rs.radixSort(data);
         System.out.println(Arrays.toString(data));
         System.out.println(rs.counter);
     }
